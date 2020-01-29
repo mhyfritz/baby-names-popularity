7cabb5ff4504950b4982c5f58c44f3618f9527cb
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2020-01-29 09:59:48 +0100

    remove unused css import

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: [],
     theme: {
       dark: true,
       themes: {
